47f55fe9872f214e2a2a4e3d3a994163acf40044
Ranbut <Ranbut_Beater@protonmail.com>
2023-01-28 10:25:58 -0300

    fix: Funções marcadas como descontinuada em versão

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import ReactDOM from "react-dom"
-import App from "./App"
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(<App/>, document.querySelector(".root"))
+root.render(<App tab="home" />);
