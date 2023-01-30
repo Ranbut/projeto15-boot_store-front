1763895a4b107722660e1ece004bd7c1b066acbc
Ranbut <Ranbut_Beater@protonmail.com>
2023-01-30 14:04:56 -0300

    feat: Checkout layout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,3 +1,33 @@
-export default function Checkout(){
+import { CheckoutBody, FormContainer, Container, Confirm, MethodPayment, PaymentType, PaymentInfo, InputForm } from "./styles"
+import Header from "../Header"
 
+export default function Checkout(){
+    return(
+    <>
+        <Header/>
+        <CheckoutBody>
+            <Container>
+                    <h1>Informações de pagamento</h1>
+                    <FormContainer>
+                    <MethodPayment>
+                            <h4>Tipo de pagamento:</h4>
+                            <PaymentType>
+                                <input type="radio" value="boleto" name="boleto" defaultChecked={true} /> Gerar Boleto
+                            </PaymentType>
+                            <PaymentType>
+                                <input type="radio" value="pix" name="pix" /> Gerar Pix
+                            </PaymentType>
+                        </MethodPayment>
+                        <PaymentInfo>
+                            <p>Confimar informações:</p>
+                            <InputForm type="name" placeholder="Digite seu nome completo" name="nome" value="" focus required />
+                            <InputForm type="email" placeholder="Digite seu email" name="email" value="" focus required />
+                            <InputForm type="password" placeholder="Digite sua senha" name="password" value="" focus required />
+                            <Confirm type="submit">Confirmar Pagamento</Confirm>
+                        </PaymentInfo>
+                    </FormContainer>
+                </Container>
+        </CheckoutBody>
+    </>
+    )
 }
